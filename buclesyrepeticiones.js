950e33325ee9d37dadc6aaa89b4e65c0083bf787
jntajes <joshuatajessosa@gmail.com>
2023-08-22 22:09:45 -0300

    practica clase

---
+++ buclesyrepeticiones.js
@@ -0,0 +1,22 @@
+n = 5
+for (let i = n + 1; i <= n + 10; i++) {
+    console.log(i)
+}
+
+console.log('<========Punto 2========>')
+
+for (let i = 5; i <= 20; i += 3) {
+    console.log(i)
+}
+
+console.log('<========Punto 3========>')
+
+let x = 0
+
+for (let i = 0; i <= 100; i++) {
+    x = i + x
+}
+
+console.log(x)
+
+console.log('<========Punto 4========>')
